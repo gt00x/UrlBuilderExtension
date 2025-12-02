1d3c9aeed3549c599f34e07fd1b3ebca09677d78
gt00x <gt00x-1@yahoo.de>
2025-12-02 15:41:54 +0000

    Another try to fix : App.tsx : /index.html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,31 @@
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import UrlBuilder from "@/pages/UrlBuilder";
 
+function Router() {
+  return (
+    <Switch>
+      {/* handle / and /index.html explicitly */}
+      <Route path="/" component={UrlBuilder} />
+      <Route path="/index.html" component={UrlBuilder} />
+      {/* catch-all: any other path -> UrlBuilder */}
+      <Route component={UrlBuilder} />
+    </Switch>
+  );
+}
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <UrlBuilder />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
 
 export default App;
-
