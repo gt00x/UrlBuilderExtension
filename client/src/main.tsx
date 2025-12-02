628a8cc50fc489f7966c7aa013b8c6f6c8a2fdf3
gt00x <gt00x-1@yahoo.de>
2025-12-02 12:04:46 +0000

    fix the main popup showing only error due to routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,20 @@
+/*  original code
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UrlBuilder from "./pages/UrlBuilder";
+
+// keep your existing global CSS import here, e.g.:
+import "./index.css"; // or "./globals.css" or whatever you had before
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <UrlBuilder />
+  </React.StrictMode>
+);
