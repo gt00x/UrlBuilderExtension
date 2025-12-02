59fba861ece2f61f925f66f62b06edf7253f3c3a
gt00x <gt00x-1@yahoo.de>
2025-12-02 12:42:16 +0000

    removed original code in main.tsx as somehow the fix did not show any improvement

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,3 @@
-/*  original code
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import UrlBuilder from "./pages/UrlBuilder";
