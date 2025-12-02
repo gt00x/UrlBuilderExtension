441f4a9e44416fd119f150732325773233656702
gt00x <gt00x-1@yahoo.de>
2025-12-02 17:04:45 +0000

    another try to fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import UrlBuilder from "@/pages/UrlBuilder";
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <UrlBuilder />
       </TooltipProvider>
     </QueryClientProvider>
   );
